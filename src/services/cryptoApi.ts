0a2627b5dfcbd79b2e81d84dadea8f3e7975a5a8
unknown <andreyxdd@yandex.ru>
2022-05-31 01:10:28 -0600

    Initialized react ts project
    Created main comonents
    
    Configured redux with the toolkit

---
+++ src/services/cryptoApi.ts
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-access-token': 'coinranking905ab4547db0d22ac12475152245f36814bf6182bff1e321'
+}
+
+const baseUrl = 'https://api.coinranking.com/v2';
+
+const createRequest = (url: string) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery, useGetCryptoHistoryQuery } = cryptoApi;
---
+++ src/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { cryptoApi } from './services/cryptoApi';
+import { cryptoNewsApi } from './services/cryptoNewsApi';
+
+const store =  configureStore({
+  reducer: {
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
