6e69c1e044a58755fd83e192e1218789127bb9ca
Andrey Volkov <andreyxdd@yandex.ru>
2022-06-01 16:06:42 -0600

    Updated crypto news API
    Corrected navbar and main container styles
    
    Other minro udpates

--- src/api/cryptoNewsApi.ts
+++ src/api/cryptoNewsApi.ts
@@ -1,14 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsApiHeaders = {
-  'x-bingapis-sdk': 'true',
-  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-  'x-rapidapi-key': 'znNqsrfNETmshy29qxz6anjCvmU2p11pRcFjsnpm3MEJIq3HUV'
+  'X-BingApis-SDK': 'true',
+  'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+  'X-RapidAPI-Key': '45552abf68mshe418f99c7e18f66p1a063fjsne4fd35f3253c'
 };
 
 const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
 
-const createRequest = (url: string) => ({ url, headers: cryptoNewsApiHeaders });
+const createRequest = (url: string) => ({
+  url,
+  method: 'GET',
+  headers: cryptoNewsApiHeaders
+});
 
 export const cryptoNewsApi = createApi({
   reducerPath: 'cryptoNewsApi',
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,9 +36,9 @@ const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar src={icon} size="large" />
+        <Avatar src={icon} size={80} />
         <Typography.Title level={2} className="logo">
-          <Link to="/">Cryptoverse</Link>
+          <Link to="/">Cryptolog</Link>
         </Typography.Title>
         <Button
           className="menu-control-container"
@@ -53,9 +53,9 @@ const Navbar = () => {
             <Link to="/">Home</Link>
           </Menu.Item>
           <Menu.Item icon={<FundOutlined />}>
-            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            <Link to="/cryptocurrencies">Crypto Top-100</Link>
           </Menu.Item>
-          <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Menu.Item disabled icon={<MoneyCollectOutlined />}>
             <Link to="/exchanges">Exchanges</Link>
           </Menu.Item>
           <Menu.Item icon={<BulbOutlined />}>
