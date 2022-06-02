e8a10d838cc0bb0aea4d4e80ba014f0e238a35f7
Andrey Volkov <andreyxdd@yandex.ru>
2022-06-02 10:36:53 -0600

    Testing husky

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react-scripts" />
 
-declare module "*.png" {
+declare module '*.png' {
    const value: any;
    export = value;
 }
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 
 import { cryptoApi } from './api/cryptoApi';
 import { cryptoNewsApi } from './api/cryptoNewsApi';
 
-const store =  configureStore({
+const store = configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
     [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
